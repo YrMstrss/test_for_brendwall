e0888c853ff9e19eb0dd98e89dc2edf153209b57
ValeryYaku <v.yaku.bmstu@gmail.com>
2024-09-11 16:14:30 +0300

    created model 'Product'

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    """
+    Модель продукта
+    """
+    name = models.CharField(max_length=100, verbose_name='название')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='цена')
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
