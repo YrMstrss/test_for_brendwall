1549047413814b0faac9fca6ce6e8fef319aac95
ValeryYaku <v.yaku.bmstu@gmail.com>
2024-09-11 16:53:18 +0300

    created APIView and url for getting list of all products

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from products.apps import ProductsConfig
-from products.views import ProductCreateAPIView
+from products.views import ProductCreateAPIView, ProductListAPIView
 
 app_name = ProductsConfig.name
 
 urlpatterns = [
     path('create/', ProductCreateAPIView.as_view(), name='create-product'),
+    path('list/', ProductListAPIView.as_view(), name='list-product'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 
+from products.models import Product
 from products.serializers import ProductSerializer
 
 
@@ -8,3 +9,12 @@ class ProductCreateAPIView(generics.CreateAPIView):
     Контроллер для создания продукта
     """
     serializer_class = ProductSerializer
+
+
+class ProductListAPIView(generics.ListAPIView):
+    """
+    Контроллер для получения списка продуктов
+    """
+
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
